dce9ea25bae212234ce2e07fea95f42a26c4e45d
Aymen <a.nhaila@aui.ma>
2025-05-21 18:18:10 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -342,7 +342,7 @@ function App() {
     };
     fetchPokemonDetails();
   }, [currentPokemonInfo]);
-
+  //test
   // Add event listener for Pokedex data updates
   useEffect(() => {
     const handlePokedexDataUpdate = (event) => {
